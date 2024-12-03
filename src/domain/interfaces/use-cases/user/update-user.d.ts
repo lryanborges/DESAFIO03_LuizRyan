c7a41527862ff311208a1a4963a11312793fb96d
Ryan Borges <ryanborgeslockser@gmail.com>
2024-12-03 04:13:09 -0300

    test: unit test update user

--- src/domain/interfaces/use-cases/user/update-user.d.ts
+++ src/domain/interfaces/use-cases/user/update-user.d.ts
@@ -1,5 +1,6 @@
+import User from "../../../models/user.ts";
 import { UpdateUserDTO } from "../../dtos/user/update-user-dto.js";
 
 export interface UpdateUser {
-    exec(id: string, data: UpdateUserDTO): Promise<void>
+    exec(id: string, data: UpdateUserDTO): Promise<User>
 }
--- src/domain/use-cases/user/update-user.ts
+++ src/domain/use-cases/user/update-user.ts
@@ -1,26 +1,27 @@
 import { HttpError } from "../../../framework/express/exceptions/http-error.js";
-import { ResourceAlredyExistsError } from "../../../framework/express/exceptions/resource-alredy-exists-error.js";
+import { ResourceNotFoundError } from "../../../framework/express/exceptions/resource-not-found-error.js";
 import { UpdateUserDTO } from "../../interfaces/dtos/user/update-user-dto.js";
 import { UpdateUser } from "../../interfaces/use-cases/user/update-user.js";
+import User from "../../models/user.js";
 import { updateUserParser } from "../../parsers/user/update-user-parser.js";
 import { uuidParser } from "../../parsers/uuid-parser.js";
 import { userRepository } from "../../repositories/user-repository.js";
 import * as argon from "argon2";
 
 export const updateUser = new class implements UpdateUser {
-    async exec(id: string, data: UpdateUserDTO): Promise<void> {
+    async exec(id: string, data: UpdateUserDTO): Promise<User> {
         uuidParser.parse(id)
         const { email, password, name } = updateUserParser.parse(data)
         const userExisting = await userRepository.findOne({ where: { id } });
 
         if (!userExisting) {
-            throw new ResourceAlredyExistsError("User");
+            throw new ResourceNotFoundError("User");
         }
 
         const emailExists = await userRepository.findOne({ where: { email } });
 
         if (emailExists && emailExists.id !== id) {
-            throw new HttpError("Conflict alrery exists a user with this email", 409);
+            throw new HttpError("Conflict already exists a user with this email", 409);
         }
 
         if (password) {
@@ -33,5 +34,7 @@ export const updateUser = new class implements UpdateUser {
         if(email) userExisting.email = email;
 
         await userRepository.save(userExisting);
+
+        return userExisting;
     }
 }
