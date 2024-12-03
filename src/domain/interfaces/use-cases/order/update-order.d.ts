02281bcb73f69ebf73cd496d086adaf383e44cee
Ryan Borges <ryanborgeslockser@gmail.com>
2024-12-03 11:18:00 -0300

    test: unit test update order

--- src/domain/interfaces/use-cases/order/update-order.d.ts
+++ src/domain/interfaces/use-cases/order/update-order.d.ts
@@ -1,5 +1,6 @@
+import Order from "../../../models/order.ts";
 import { UpdateOrderDTO } from "../../dtos/order/update-order-dto.js";
 
 export interface UpdateOrder {
-    exec(id: string, data: UpdateOrderDTO): Promise<void>
+    exec(id: string, data: UpdateOrderDTO): Promise<Order>
 }
