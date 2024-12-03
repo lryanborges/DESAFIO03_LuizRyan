618edcc4215e060a34cdfae4bdfc40ee28e3c9a6
Ryan Borges <ryanborgeslockser@gmail.com>
2024-12-03 06:33:05 -0300

    fix: add beforeAll in delete user

--- src/test/use-cases/user/delete-user.test.ts
+++ src/test/use-cases/user/delete-user.test.ts
@@ -1,14 +1,25 @@
+import { CreateUserDTO } from "../../../domain/interfaces/dtos/user/create-user-dto.js";
+import { createUser } from "../../../domain/use-cases/user/create-user.js";
 import { deleteUser } from "../../../domain/use-cases/user/delete.user.js";
-import { findManyUser } from "../../../domain/use-cases/user/find-many-user.js";
 import { v4 as uuidv4 } from "uuid";
 
 describe("Delete user", () => {
+
+    let idToDelete;
+
+    beforeAll(async () => {
+        const createUserData: CreateUserDTO = {
+            name: "Ryan",
+            email: Date.now() + "@gmail.com",
+            password: Date.now() + "123"
+        }
+
+        const user = await createUser.exec(createUserData);
+        idToDelete = user.id;
+      });
+
     test("Should delete a user successfully", async () => {
-        const userToDelete = await findManyUser.exec({ name: "Ryan"}); // pra n excluir o administrador
-
-        expect(userToDelete).not.toBeNull();
-
-        const deletedUser = await deleteUser.exec(userToDelete.data.at(0).id);
+        const deletedUser = await deleteUser.exec(idToDelete);
 
         expect(deletedUser.excludedAt).not.toBeNull();
     });
